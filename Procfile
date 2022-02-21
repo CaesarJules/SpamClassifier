e15bc2299f216a0e20418c489ffe81d9d0c748ce
CaesarJules <42262367+CaesarJules@users.noreply.github.com>
2022-02-21 06:18:14 -0500

    restructured repo.

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: sh setup.sh && streamlit run spam_classifier.py
+web: sh setup.sh && streamlit run ./scripts/spam_classifier.py
--- scripts/spam_classifier.py
+++ scripts/spam_classifier.py
@@ -16,9 +16,9 @@ from functions import *
 url="http://44.202.42.66:8000/api/status/"
 #Get the current file's directory
 path = os.path.dirname(__file__)
-filepath = os.path.join(path, 'data/sample_annotated.csv')
+filepath = os.path.join(path, '../data/sample_annotated.csv')
 #Create logo filepath
-logo = os.path.join(path, 'data/plots/logo.png')
+logo = os.path.join(path, '../data/plots/logo.png')
 st.set_page_config(page_title='Spam Detector', page_icon = logo, layout = 'wide', initial_sidebar_state = 'auto')
 
 def test_url_availability(url):
